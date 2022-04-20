d1ed4ce05b3154d362c6e89469e06dc5decbf639
liuxinyi-yuhang <1029301987@qq.com>
2022-04-20 14:26:44 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,14 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-04-20 09:16:42
+ * @LastEditTime: 2022-04-20 13:55:26
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \spc-web-admin\README.md
+-->
+#### 介绍
+
+基于 vue3.x + CompositionAPI + typescript + vite + element plus 开发的SPC质量控制管理平台
+
+#### 项目启动
+<a>asdasd</a>
---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-04-20 09:16:42
+ * @LastEditTime: 2022-04-20 13:32:31
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue-next-admin\src\views\home\index.vue
+-->
+<template>
+	<div>
+		这是{{name}}
+	</div>
+</template>
+
+<script lang="ts" setup>
+const name: string = `首页`
+</script>
+
+<style lang="scss" scoped></style>
